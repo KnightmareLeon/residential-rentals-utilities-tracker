3f516db491e9d547e232a965b1802e5ef210beb7
KimGab <nasayaokim@gmail.com>
2025-05-09 21:25:01 +0800

    fix(controllers): Fixed fetchUtilityDashboard

--- src/views/components/UtilityDashboard.py
+++ src/views/components/UtilityDashboard.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QFrame, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QButtonGroup, QComboBox
 from PyQt6.QtGui import QFont, QIcon
-from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtCore import Qt, QSize, QDate
+from datetime import datetime
 
 from src.utils.sampleDataGenerator import generateRandomUtilityData
 from src.views.widgets.UtilityChartWidget import UtilityChartWidget
@@ -56,7 +57,7 @@ class UtilityDashboard(QFrame):
         mainLayout.addLayout(summaryLayout)
 
         # === Utility Chart ===
-        data, _ = DashboardController.fetchUtilityDashboard(3, 1)
+        data, _ = DashboardController.fetchUtilityDashboard(3, datetime.now())
         self.chartWidget = UtilityChartWidget(data, "Total Utilities Cost of All Units", mainWindow=self.mainWindow)
         mainLayout.addWidget(self.chartWidget)
 
